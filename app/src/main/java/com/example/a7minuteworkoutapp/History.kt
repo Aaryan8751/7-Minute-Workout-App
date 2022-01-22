2d3da5789c8a9eb983a32bb1a9ffd18ea0c51904
Aaryan Sharma <60098288+Aaryan8751@users.noreply.github.com>
2022-01-23 04:39:20 +0530

    History, SQLite, Memory Storage

--- app/src/main/java/com/example/a7minuteworkoutapp/History.kt
+++ app/src/main/java/com/example/a7minuteworkoutapp/History.kt
@@ -2,7 +2,11 @@ package com.example.a7minuteworkoutapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.a7minuteworkoutapp.databinding.ActivityHistoryBinding
+import kotlinx.android.synthetic.main.activity_history.*
 
 class History : AppCompatActivity() {
 
@@ -26,6 +30,28 @@ class History : AppCompatActivity() {
             onBackPressed()
         }
 
+        getAllCompletedDates()
+
+    }
+
+    private fun getAllCompletedDates(){
+        val dbHandler = SqliteOpenHelper(this,null)
+        val allCompletedDatesList = dbHandler.getAllCompleteDatesList()
+
+        if(allCompletedDatesList.size>0){
+            binding?.tvHistory?.visibility = View.VISIBLE
+            binding?.rvHistory?.visibility = View.VISIBLE
+            binding?.tvNoDataAvailable?.visibility = View.GONE
+
+            binding?.rvHistory?.layoutManager = LinearLayoutManager(this)
+            val historyAdapter = HistoryAdaptor(this,allCompletedDatesList)
+            binding?.rvHistory?.adapter = historyAdapter
+        }
+        else{
+            binding?.tvHistory?.visibility = View.GONE
+            binding?.rvHistory?.visibility = View.GONE
+            binding?.tvNoDataAvailable?.visibility = View.VISIBLE
+        }
     }
 
     override fun onDestroy() {
--- app/src/main/java/com/example/a7minuteworkoutapp/finish.kt
+++ app/src/main/java/com/example/a7minuteworkoutapp/finish.kt
@@ -2,7 +2,10 @@ package com.example.a7minuteworkoutapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.a7minuteworkoutapp.databinding.ActivityFinishBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class finish : AppCompatActivity() {
 
@@ -27,5 +30,22 @@ class finish : AppCompatActivity() {
             finish()
         }
 
+        addDateToDatabase()
+
     }
+
+    private fun addDateToDatabase(){
+        val calender = Calendar.getInstance()
+        val dateTime = calender.time
+        Log.i("Date:"," "+ dateTime)
+
+        val sdf = SimpleDateFormat("dd MMM yyyy HH:mm:ss",Locale.getDefault())
+        val date = sdf.format(dateTime)
+
+        val dbHandler = SqliteOpenHelper(this,null)
+        dbHandler.addDate(date)
+        Log.i("Date: ","Added")
+    }
+
+
 }
